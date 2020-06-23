37c025ed7c8fc164a7a8b985626067a31f3c5f0c
TheodorKnab <32791723+TheodorKnab@users.noreply.github.com>
2020-06-23 17:17:57 +0200

    created main and init

--- runScript.py
+++ runScript.py
@@ -5,20 +5,20 @@ import numpy as np
 from copy import deepcopy
 robocodePath = 'D:/FHTech/Sem2/ARL/Robocode/INSTALL/'
 dataPath = os.path.dirname(sys.argv[0]) + '/out/production/RobocodeDeepQLearning/ARL/Rl_nn.data/'
-battlePath = 'battles/' #path startign at the robocode directory
-battleFileName = 'NNTrain.battle'
+battleFileName = 'battles/NNTrain.battle'
 
 inputNeurons = 7
 hiddenLayerNeurons = 10
 outputNeurons = 1
 populationSize = 10
 randomWeightStandardDeviation = 5
-hyperparamAmount = 10
+hyperparamAmount = 11
 hyperparamStandardDeviation = 10
 # hyperparams:
 # alpha,
 # gamma,
 # rho,
+# epsilon
 # hitBulletReward,
 # hitByBulletReward,
 # hitRobotReward,
@@ -59,7 +59,7 @@ def clamp01(toClamp):
     return clamp(toClamp, 0, 1)
 
 def saveWeights(weights, filename):
-    f = open(dataPath + filename, "w")
+    f = open(dataPath + filename, "w+")
     for j in range(len(weights)):
 
         f.write(str(weights[j][0]))
@@ -73,17 +73,18 @@ def loadWeights(filename):
         return [[float(x) for x in line.split("    ")] for line in f]
 
 def saveHyperparams(hyperParams, filename):
-    f = open(dataPath + filename, "w")
-    f.write(str(clamp01(hyperParams[0])) + "\n")            # alpha,
-    f.write(str(clamp01(hyperParams[1])) + "\n")            # gamma,
-    f.write(str(clamp01(hyperParams[2])) + "\n")            # rho,
-    f.write(str(hyperParams[3]) + "\n")                     # hitBulletReward,
-    f.write(str(hyperParams[4]) + "\n")                     # hitByBulletReward,
-    f.write(str(hyperParams[5]) + "\n")                     # hitRobotReward,
-    f.write(str(hyperParams[6]) + "\n")                     # hitwallReward,
-    f.write(str(hyperParams[7]) + "\n")                     # onDeathReward,
-    f.write(str(hyperParams[8]) + "\n")                     # onWinReward,
-    f.write(str(int(clamp(hyperParams[9], 5, 30))) + "\n")  # hiddenLayerNeurons
+    f = open(dataPath + filename, "w+")
+    f.write(str(hyperParams[0]) + "\n")                     # alpha,
+    f.write(str(hyperParams[1]) + "\n")                     # gamma,
+    f.write(str(hyperParams[2]) + "\n")                     # rho,
+    f.write(str(hyperParams[3]) + "\n")                     # epsilon,
+    f.write(str(hyperParams[4]) + "\n")                     # hitBulletReward,
+    f.write(str(hyperParams[5]) + "\n")                     # hitByBulletReward,
+    f.write(str(hyperParams[6]) + "\n")                     # hitRobotReward,
+    f.write(str(hyperParams[7]) + "\n")                     # hitwallReward,
+    f.write(str(hyperParams[8]) + "\n")                     # onDeathReward,
+    f.write(str(hyperParams[9]) + "\n")                     # onWinReward,
+    f.write(str(hyperParams[10]) + "\n")  # hiddenLayerNeurons
     f.close()
 
 def loadHyperparams(filename):
@@ -94,31 +95,36 @@ def loadHyperparams(filename):
 def generateWeights(inputNeurons, outputNeurons, weights_hidden=None, weights_output=None):
     # Generate random weights_hidden array with Normal distribution
     for i in range(populationSize):
-        weights_hidden = [[random.gauss(0, randomWeightStandardDeviation) for k in range(inputNeurons + 1)] for j in range(int(robots[i].hyperparams[9]))]
+        weights_hidden = [[random.gauss(0, randomWeightStandardDeviation) for k in range(inputNeurons + 1)] for j in range(int(robots[i].hyperparams[10]))]
         saveWeights(weights_hidden, str(i) + "weights_hidden.txt")
 
-        weights_output = [[random.gauss(0, randomWeightStandardDeviation) for k in range(int(robots[i].hyperparams[9]) + 1)] for j in range(outputNeurons)]
+        weights_output = [[random.gauss(0, randomWeightStandardDeviation) for k in range(int(robots[i].hyperparams[10]) + 1)] for j in range(outputNeurons)]
         saveWeights(weights_output, str(i) + "weights_output.txt")
 
+#genereates Hyperparameter, and saves them in robots and in files
 def generateHyperparams():
-    hyperParams = [populationSize]
+    hyperParams = [hyperparamAmount]
     for i in range(populationSize):
         hyperParams = [random.gauss(0, hyperparamStandardDeviation) for k in range(hyperparamAmount)]
         #scale to better fit 0 - 1
-        hyperParams[0] = (hyperParams[0] / hyperparamStandardDeviation) * 0.3 + 0.5         # alpha,
-        hyperParams[1] = (hyperParams[1] / hyperparamStandardDeviation) * 0.3 + 0.5         # gamma,
-        hyperParams[2] = (hyperParams[2] / hyperparamStandardDeviation) * 0.00005 + 0.0001  # rho,
-        hyperParams[9] =  hyperParams[9] + 10                                               # hiddenLayerNeurons,
+        hyperParams[0] = clamp01(abs(hyperParams[0] / hyperparamStandardDeviation) * 0.3 + 0.5)         # alpha,
+        hyperParams[1] = clamp01(abs(hyperParams[1] / hyperparamStandardDeviation) * 0.3 + 0.5)         # gamma,
+        hyperParams[2] = clamp01(abs(hyperParams[2] / hyperparamStandardDeviation) * 0.00005 + 0.0001)  # rho,
+        hyperParams[3] = random.random()                                                                # epsilon,
+        hyperParams[10] = int(clamp(abs(hyperParams[10] + 10), 2, 100))                                 # hiddenLayerNeurons,
+        robots[i].hyperparams = hyperParams
         saveHyperparams(hyperParams, str(i) + "hyperparams.txt")
+
 
 def loadAllHyperparams():
     hyperParams = [[0 for x in range(hyperparamAmount)] for y in range(populationSize)]
     for i in range(populationSize):
         hyperParams[i] = loadHyperparams(str(i) + "hyperparams.txt")
+        robots[i].hyperparams = hyperParams[i]
     return hyperParams
 
 def calculateFitness(filename):
-    read_data = ['lol'] * 2
+    read_data = [str] * 2
     with open(dataPath + filename) as f:
         f.readline()
         f.readline()
@@ -141,8 +147,12 @@ def calculateFitness(filename):
 
 def runRoboCode(generations, battlePathAndName, resultPathandName):
     for x in range(generations):
-        p1 = subprocess.Popen('java -Xmx512M -Dsun.io.useCanonCaches=false -DPARALLEL=true -cp libs/robocode.jar robocode.Robocode -battle ./' + battlePathAndName + ' -results ' + resultPathandName + ' -tps 1000000 -nodisplay', cwd = robocodePath)
-        p1.wait()
+        if (resultPathandName == ''):
+            p1 = subprocess.Popen('java -Xmx512M -Dsun.io.useCanonCaches=false -DPARALLEL=true -cp libs/robocode.jar robocode.Robocode -battle ./' + battlePathAndName + ' -tps 1000000 -nodisplay', cwd = robocodePath)
+            p1.wait()
+        else:
+            p1 = subprocess.Popen('java -Xmx512M -Dsun.io.useCanonCaches=false -DPARALLEL=true -cp libs/robocode.jar robocode.Robocode -battle ./' + battlePathAndName + ' -results ' + resultPathandName + ' -tps 1000000 -nodisplay', cwd = robocodePath)
+            p1.wait()
         print("gen: " + str(x+1) + " of " + str(generations) + " done")
 
 
@@ -228,7 +238,7 @@ def crossover(father, mother):
 
 def makeEvolution(parents):
     selectedParents = selectParents(parents)
-    nextGen = [0] * populationSize
+    nextGen = [Robot] * populationSize
     nextGen[0] = selectedParents[0]
     nextGen[1] = selectedParents[1]
 
@@ -258,11 +268,48 @@ def makeEvolution(parents):
 
     return nextGen
 
-robots = [Robot(0)] * populationSize
+
+def resetConfig():
+    f = open(dataPath + "config.txt","w+")
+    f.write("0\n")
+    f.close()
+
+def init(populationSize):
+    generateHyperparams()
+    generateWeights(inputNeurons,outputNeurons)
+    resetConfig()
+
+def main(generations):
+
+    for r in robots:
+        r.loadHyperparams()
+
+    for i in range(generations):
+        for j in range(populationSize):
+            runRoboCode(1,robocodePath + battleFileName, '')
+            robots[j].hyperparams[3] = 1
+            robots[j].saveHyperparams()
+            runRoboCode(1,robocodePath + battleFileName, str(j) + "result.txt")
 
 
-def init():
-    print()
+        for r in robots:
+            r.loadFitness()
 
-def run():
-    print()
+        robots = makeEvolution(robots)
+
+        for r in robots:
+            r.saveHyperparams()
+        resetConfig()
+
+
+
+
+
+robots = [Robot] * populationSize
+for i in range(0,populationSize):
+    robots[i] = Robot(i)
+
+
+
+
+main(1)
